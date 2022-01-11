6af7076394221e46fa50457cad58743f80af539b
APSsmd <codean13@gmail.com>
2022-01-11 18:43:50 +0700

    close #11 - added treatment record (Untested)

---
+++ backend/controller/treatment_controller.go
@@ -0,0 +1,71 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut64/team13/entity"
+)
+
+// POST /treatment
+func CreateTreatment(c *gin.Context) {
+	var treatment entity.Treatment
+
+	// TODO:
+	// var screening
+	// var remedytype
+	var user entity.User
+
+	if err := c.ShouldBindJSON(&treatment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", treatment.DentistID).First(&user); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No user found with association ID"})
+		return
+	}
+
+	entity.DB().Joins("Role").Find(&user)
+	if user.Role.Name != "Dentist" {
+		c.JSON(http.StatusForbidden, gin.H{"error": "user have no authoize"})
+		return
+	}
+
+	// TODO:
+	// find screening with given id
+	// find remedytype with given id
+
+	data := entity.Treatment{
+		PrescriptionRaw:  treatment.PrescriptionRaw,
+		PrescriptionNote: treatment.PrescriptionNote,
+		ToothNumber:      treatment.ToothNumber,
+		Date:             treatment.Date,
+		// create with assosiation ---
+		// TODO:
+		// screening entity
+		// remedytype entity
+		Dentist: user,
+	}
+
+	if err := entity.DB().Create(&data).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": data})
+}
+
+// GET /treatment
+func ListTreatmentRecord(c *gin.Context) {
+	var treatments []entity.Treatment
+
+	// TODO:
+	// preload other association
+	if err := entity.DB().Find(&treatments).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": treatments})
+}
