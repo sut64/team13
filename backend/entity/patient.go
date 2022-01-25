84d026ed97dac71d40556324b088b94623de4c08
Infanna <patcharachart.ji@gmail.com>
2022-01-25 13:23:29 +0700

    add patient.go close #17

---
+++ backend/entity/patient.go
@@ -0,0 +1,59 @@
+package entity
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+
+//ระบบย่อย ระบบบันทึกเวชระเบียน
+type Job struct {
+	gorm.Model
+	Name string
+
+	Patients []Patient `gorm:"foreignKey:JobID"`
+}
+
+type Insurance struct {
+	gorm.Model
+	Name   string
+	Detail string
+
+	Patients []Patient `gorm:"foreignKey:InsuranceID"`
+}
+
+type Sex struct {
+	gorm.Model
+	Name string
+
+	Patients []Patient `gorm:"foreignKey:SexID"`
+}
+
+type Patient struct {
+	gorm.Model
+	Firstname string
+	Lastname  string
+	Birthday  time.Time
+	IDcard    string `gorm:"uniqueIndex"`
+	Tel       string
+	Wight	  float32
+	Height	  float32
+	Time      time.Time
+
+	UserNurseID *uint
+	UserNurse   User
+
+	JobID *uint
+	Job   Job
+
+	InsuranceID *uint
+	Insurance   Insurance
+
+	SexID *uint
+	Sex   Sex
+  
+}
+
+
+
+
