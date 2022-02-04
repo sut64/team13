9677d64588754f4acf4c1278962673ca47af6de0
PITCH <kantapitch@MacBook-Pro-khxng-PITCH.local>
2022-02-04 15:59:49 +0700

    close #117

--- backend/controller/screening.go
+++ backend/controller/screening.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/asaskevich/govalidator"
 	"github.com/sut64/team13/entity"
 
 	"github.com/gin-gonic/gin"
@@ -60,6 +61,11 @@ func CreateScreening(c *gin.Context) {
 		Queue:     screening_record.Queue,     //ลำดับการเข้ารับการรักษา
 		Date:      time.Now(),                 //ดึงเวลาปัจจุบัน
 
+	}
+	//ขั้นตอนการ validate ที่นำมาจาก unit test
+	if _, err := govalidator.ValidateStruct(scr); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	//13:บันทึก()
 	if err := entity.DB().Create(&scr).Error; err != nil {
