0b5b31422c33307019ab4ef6b259db5dab976be2
Infanna <patcharachart.ji@gmail.com>
2022-02-09 01:17:26 +0700

    Navber signout_Set_Username

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,9 +4,8 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { Fade, Menu, MenuItem } from '@material-ui/core';
+import { Fade, Menu} from '@material-ui/core';
 
 import MenuIcon from '@material-ui/icons/MenuOpenOutlined';
 import LogoutIcon from "@material-ui/icons/ExitToAppOutlined";
@@ -17,14 +16,23 @@ import CreateIcon from "@material-ui/icons/AddCircleOutlineOutlined";
 import Grid from "@material-ui/core/Grid";
 import PaidIcon from '@mui/icons-material/Paid';
 import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
-import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
 import ContentPasteSearchOutlinedIcon from '@mui/icons-material/ContentPasteSearchOutlined';
 import MedicationIcon from '@mui/icons-material/Medication';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import FactCheckOutlinedIcon from '@mui/icons-material/FactCheckOutlined';
+import { UserInterface } from "../models/IUser";
+import { useEffect } from "react";
 
-
+import Button from '@mui/material/Button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -86,14 +94,22 @@ const PaymentCreate = () => {
   window.location.href = "/paymentcreate";
 };
 
+
+
 const CreateMecRecord = () => {
   window.location.href = "/CreateMecRecord";
 };
 
+
+
+
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [Useronline, setUseronline] = React.useState<UserInterface>();
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
@@ -101,6 +117,74 @@ export default function ButtonAppBar() {
     setAnchorEl(null);
   };
 
+
+
+  function getUseronline() {
+    const UserID = localStorage.getItem("uid")
+    const apiUrl = `http://localhost:8080/users/${UserID}`;
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+    };
+  
+    fetch(apiUrl, requestOptions)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log("Combobox_Useronline_navber", res)
+        if (res.data) {
+          setUseronline(res.data);
+        } else {
+          console.log("else");
+        }
+      });
+  }
+  
+  useEffect(() => {
+    getUseronline();
+  }, []);
+
+  //---------------
+  const [open_signout, setOpen_signout] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen_signout((prevOpen_signout) => !prevOpen_signout);
+  };
+
+  const handleClose_signout = (event: Event | React.SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen_signout(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen_signout(false);
+    } else if (event.key === 'Escape') {
+      setOpen_signout(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen_signout = React.useRef(open_signout);
+  React.useEffect(() => {
+    if (prevOpen_signout.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+    
+    prevOpen_signout.current = open_signout;
+  }, [open_signout]);
+
+  //---------------
 
   return (
     <div className={classes.root}>
@@ -162,15 +246,51 @@ export default function ButtonAppBar() {
             </Button>
         </Grid>
 
-          <Button style={{ float: "right" }}
-            className={classes.Signouticons}
-            endIcon={<LogoutIcon />}
-            onClick={signout}
-            variant="outlined"
-
-            color="inherit">
-            ออกจากระบบ
-          </Button>
+        <Button
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open_signout ? 'composition-menu' : undefined}
+          aria-expanded={open_signout ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+          sx={{color : "#F4EEDC",width : "320px", fontSize: 17}}
+        >
+          <AssignmentIndIcon/>&nbsp;&nbsp;{Useronline?.Firstname} {Useronline?.Lastname}&nbsp;<ArrowDropDownIcon />
+        </Button>
+        <Popper
+          open={open_signout}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-end"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose_signout}>
+                  <MenuList
+                    autoFocusItem={open_signout}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  
+                  >     
+                    <MenuItem onClick={signout} sx={{width:"220px"}}><LogoutIcon/>ออกจากระบบ </MenuItem>
+                    
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+          
         </Toolbar>
       </AppBar>
     </div>
