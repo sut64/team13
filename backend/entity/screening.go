ed205205f64065bbe2605907e3d7fc95d0bad234
PITCH <kantapitch@MacBook-Pro-khxng-PITCH.local>
2022-02-09 19:55:04 +0700

    close #48

--- backend/entity/screening.go
+++ backend/entity/screening.go
@@ -3,6 +3,7 @@ package entity
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
@@ -18,7 +19,24 @@ type Screening struct {
 	MedicalProductID *uint
 	MedicalProduct   MedicalProduct `gorm:"references:id" valid:"-"`
 	//3 type diff
-	Illnesses string `valid:"required~illnesses cannot be blank"`
-	Queue     uint
-	Date      time.Time
+	Illnesses string    `valid:"required~illnesses cannot be blank"`
+	Queue     int       `gorm:"uniqueIndex" valid:"screening_positive~Queue must not be negative, required~Queue must not be zero or empty"`
+	Date      time.Time `valid:"past~Screening time not be a future"`
+}
+
+func init() {
+	govalidator.CustomTypeTagMap.Set("past", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		now := time.Now()
+		return now.After(t)
+	})
+	govalidator.CustomTypeTagMap.Set("future", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		now := time.Now()
+		return now.Before(time.Time(t))
+	})
+	govalidator.CustomTypeTagMap.Set("screening_positive", func(i interface{}, context interface{}) bool {
+		num := i
+		return num.(int) > 0
+	})
 }
